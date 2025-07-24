e98d899eec5545d439404475d91f3373fdfe26ed
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-24 19:09:45 -0300

    refactor: Update AddToQueue and PopFromQueue methods to handle RinhaPendingPayment model

--- internal/services/cache/redis_client.go
+++ internal/services/cache/redis_client.go
@@ -2,10 +2,12 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/diogomassis/rinha-2025/internal/env"
+	"github.com/diogomassis/rinha-2025/internal/models"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -45,15 +47,20 @@ func (r *RinhaRedisClient) Close(ctx context.Context) error {
 	return nil
 }
 
-func (r *RinhaRedisClient) AddToQueue(ctx context.Context, queueName, value string) error {
-	_, err := r.client.LPush(ctx, queueName, value).Result()
+func (r *RinhaRedisClient) AddToQueue(ctx context.Context, queueName string, pendingPayment models.RinhaPendingPayment) error {
+	data, err := json.Marshal(pendingPayment)
+	if err != nil {
+		return fmt.Errorf("[cache] failed to marshal pending payment: %w", err)
+	}
+
+	_, err = r.client.LPush(ctx, queueName, data).Result()
 	if err != nil {
 		return fmt.Errorf("[cache] failed to add to queue: %w", err)
 	}
 	return nil
 }
 
-func (r *RinhaRedisClient) PopFromQueue(ctx context.Context, queueName string) ([]byte, error) {
+func (r *RinhaRedisClient) PopFromQueue(ctx context.Context, queueName string) (*models.RinhaPendingPayment, error) {
 	result, err := r.client.BRPop(ctx, 0, queueName).Result()
 	if err != nil {
 		return nil, fmt.Errorf("[cache] failed to pop from queue: %w", err)
@@ -61,5 +68,10 @@ func (r *RinhaRedisClient) PopFromQueue(ctx context.Context, queueName string) (
 	if len(result) < 2 {
 		return nil, fmt.Errorf("[cache] unexpected BRPop result: %v", result)
 	}
-	return []byte(result[1]), nil
+
+	var pendingPayment *models.RinhaPendingPayment
+	if err := json.Unmarshal([]byte(result[1]), pendingPayment); err != nil {
+		return nil, fmt.Errorf("[cache] failed to unmarshal pending payment: %w", err)
+	}
+	return pendingPayment, nil
 }
