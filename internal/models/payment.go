ad9ce2e829be327e2332a6a388da25cce52139e6
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-24 21:08:47 -0300

    refactor: Consolidate payment models for improved structure and readability

--- internal/models/payment.go
+++ internal/models/payment.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type RinhaPendingPayment struct {
 	CorrelationId string  `json:"correlationId"`
 	Amount        float64 `json:"amount"`
@@ -9,5 +11,45 @@ func NewRinhaPendingPayment(correlationId string, amount float64) *RinhaPendingP
 	return &RinhaPendingPayment{
 		CorrelationId: correlationId,
 		Amount:        amount,
+	}
+}
+
+type PaymentSummary struct {
+	Default  PaymentSummaryItem `json:"default"`
+	Fallback PaymentSummaryItem `json:"fallback"`
+}
+
+func NewPaymentSummary(defaultItem, fallbackItem PaymentSummaryItem) *PaymentSummary {
+	return &PaymentSummary{
+		Default:  defaultItem,
+		Fallback: fallbackItem,
+	}
+}
+
+type PaymentSummaryItem struct {
+	TotalRequests int64   `json:"totalRequests"`
+	TotalAmount   float64 `json:"totalAmount"`
+}
+
+func NewPaymentSummaryItem(totalRequests int64, totalAmount float64) *PaymentSummaryItem {
+	return &PaymentSummaryItem{
+		TotalRequests: totalRequests,
+		TotalAmount:   totalAmount,
+	}
+}
+
+type CompletedPayment struct {
+	CorrelationID string    `json:"correlationId"`
+	Amount        float64   `json:"amount"`
+	Type          string    `json:"type"`
+	ProcessedAt   time.Time `json:"processedAt"`
+}
+
+func NewCompletedPayment(correlationID string, amount float64, paymentType string, processedAt time.Time) *CompletedPayment {
+	return &CompletedPayment{
+		CorrelationID: correlationID,
+		Amount:        amount,
+		Type:          paymentType,
+		ProcessedAt:   processedAt,
 	}
 }
