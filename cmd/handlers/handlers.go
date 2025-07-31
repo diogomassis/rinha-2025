759930091cf096aa27fdfbc36fc3fdb84908a6be
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-31 17:55:42 -0300

    refactor: Update handlers to use PaymentPersistenceService for payment data management

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -2,16 +2,16 @@ package handlers
 
 import (
 	"github.com/diogomassis/rinha-2025/internal/dto"
+	"github.com/diogomassis/rinha-2025/internal/persistence"
 	chooserchecker "github.com/diogomassis/rinha-2025/internal/services/chooser-checker"
 	healthchecker "github.com/diogomassis/rinha-2025/internal/services/health-checker"
 	"github.com/gofiber/fiber/v2"
-	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var (
-	Db      *pgxpool.Pool
-	Monitor *healthchecker.HealthChecker
-	Chooser *chooserchecker.ChooserService
+	Persistence *persistence.PaymentPersistenceService
+	Monitor     *healthchecker.HealthChecker
+	Chooser     *chooserchecker.ChooserService
 )
 
 func HandlePostPayment(c *fiber.Ctx) error {
